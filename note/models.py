2ac274edbcbab6a024377b32e9946a6d8a3a3db7
reyzacaloh <reyza.abyan@gmail.com>
2021-10-30 22:03:14 +0700

    make html and css

--- note/models.py
+++ note/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    timestamp = models.DateTimeField()
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username} | {self.title}"
--- utsF02/settings.py
+++ utsF02/settings.py
@@ -56,7 +56,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'user'
+    'user',
+    'note'
 ]
 
 MIDDLEWARE = [
